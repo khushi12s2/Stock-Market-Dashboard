f506ba645fd28c25bcbb4218b5c467592bd13b24
khushi12s2 <shoumya.sundaram.aiml21@ggits.net>
2024-10-02 13:41:07 +0530

    update

--- app.py
+++ app.py
@@ -20,8 +20,11 @@ pricing_data,fundamental_data,news=st.tabs(["Pricing Data","Fundamental Data","T
 with pricing_data:
     st.header('Price Movements')
     data2 = data
-    data2['% Change'] = data['Adj Close']/data['Adj Close'].shift(1)
+    data2['% Change'] = data['Adj Close']/data['Adj Close'].shift(1)-1
+    data2.dropnone(inplace = True)
     st.write(data2)
+    annual_return = data2['% Change'].mean()*252*100
+    st.write("Annual Return is:",annual_return,"%")
 
 with fundamental_data:
     st.write("Fundamental")
