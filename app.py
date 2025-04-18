d11f78ba469eb386e705afcc6b68083b80ec556b
khushi12s2 <shoumya.sundaram.aiml21@ggits.net>
2025-04-18 21:05:32 +0530

    fix: standardize import statement for plotly express

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st # type: ignore
 import pandas as pd  # type: ignore
 import numpy as np  # type: ignore
 import yfinance as yf  # type: ignore
-import plotly express as px # type: ignore
+import plotly.express as px # type: ignore
 
 st.title('Stock Dashboard')
 ticker = st.sidebar.text_input('Ticker')
